489b5f3594259ded90533c861b270874c1ab3672
tchau@france.groupe.intra <thanh.chau@vidal.fr>
2025-11-24 22:29:02 +0100

    feat: add hooks index file for centralized exports
    - Create hooks/index.ts to re-export all hooks
    - Fix missing exports and organize hook imports
    - Prepares for easier hook importing across the application

---
+++ hooks/index.ts
@@ -0,0 +1,14 @@
+// Re-export all hooks for easier importing
+export { AnalysisStep, useAnalysisProgress } from './useAnalysisProgress';
+export { useDreamJournal } from './useDreamJournal';
+export { useDreamSaving, type UseDreamSavingOptions } from './useDreamSaving';
+export { useDreamStatistics } from './useDreamStatistics';
+export { useLocaleFormatting } from './useLocaleFormatting';
+export { usePrefersReducedMotion } from './usePrefersReducedMotion';
+export { useQuota } from './useQuota';
+export { useRecordingSession, type RecordingSessionResult, type UseRecordingSessionOptions } from './useRecordingSession';
+export { useSubscription } from './useSubscription';
+export { useSubscriptionInitialize } from './useSubscriptionInitialize';
+export { useSubscriptionMonitor } from './useSubscriptionMonitor';
+export { useTranslation } from './useTranslation';
+
